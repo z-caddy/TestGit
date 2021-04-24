9546c31272bb4e049c67aa28a8023768346fd811
z_caddy <zhouglassball@gmail.com>
2021-04-24 22:56:33 +0800

    update

--- src/main/java/com/YiLianMaoYi/testGit/HelloWorld.java
+++ src/main/java/com/YiLianMaoYi/testGit/HelloWorld.java
@@ -3,5 +3,6 @@ package com.YiLianMaoYi.testGit;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello GitHub");
     }
 }
